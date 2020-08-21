17db077dd23422b678447144e164467735b42184
Carmelo Ricci <carmeloricci@MacBook-Pro-di-Carmelo.local>
2020-08-21 17:15:38 +0200

    OK

--- backend/src/services/leasesServices.ts
+++ backend/src/services/leasesServices.ts
@@ -69,7 +69,7 @@ export default class LeasesServices {
         //console.log( await deviceStore.findByMacAndIp(leases.mac, leases.ip) )
         let rowdata = await deviceStore.findByMacAndIp(leases.mac, leases.ip)
         console.log(typeof rowdata, rowdata)
-console.log(rowdata[0].RowDataPacket.Ip)
+console.log(rowdata[0].RowDataPacket)
 //console.log(rowdata.RowDataPacket.Ip)
 //        temp = { Device_id: rowdata.Device_id , Tenant_id: rowdata.Tenant_id , Nome: rowdata.Nome , Ip: rowdata.Ip, Mac: rowdata.Mac}
         
