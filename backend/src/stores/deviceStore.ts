5a31a18bbd94fc81225eb284208d7901aa9f42ba
Carmelo Ricci <carmeloricci@MacBook-Pro-di-Carmelo.local>
2020-08-21 17:11:31 +0200

    ok

--- backend/src/stores/deviceStore.ts
+++ backend/src/stores/deviceStore.ts
@@ -5,68 +5,72 @@ var config = require('../../../backend/knexfile');
 var knex = require('knex')(config[process.env.NODE_ENV]);
 
 
- export default class DeviceStore {
-     constructor() { }
+export default class DeviceStore {
+    constructor() { }
 
-//     create(device: IDevice) {
-//         return knex('devices').insert(device).returning('*');
-//     }
+    //     create(device: IDevice) {
+    //         return knex('devices').insert(device).returning('*');
+    //     }
 
-//     update(device: IDevice) {
-//         const date = new Date(moment().format()).toISOString();
-//         device.updated_at = date;
-//         return knex('devices').where({ id: device.id }).update(device).returning('*');
-//     }
+    //     update(device: IDevice) {
+    //         const date = new Date(moment().format()).toISOString();
+    //         device.updated_at = date;
+    //         return knex('devices').where({ id: device.id }).update(device).returning('*');
+    //     }
 
-//     delete(id: number) {
-//         return knex('devices').where({ id }).del();
-//     }
+    //     delete(id: number) {
+    //         return knex('devices').where({ id }).del();
+    //     }
 
-//     findById(id: number): any {
-//         return knex('devices').select('*').where({ id });
-//     }
+    //     findById(id: number): any {
+    //         return knex('devices').select('*').where({ id });
+    //     }
 
-    async findByMac(mac:string) {
+    async findByMac(mac: string) {
         return await knex('Devices')
-            .where({ Mac: mac
-             })
-             .select('*')
+            .where({
+                Mac: mac
+            })
+            .select('*')
     }
 
     async create(device: IDevice) {
         return await knex('Devices').insert(device)
     }
 
-    async findByMacAndIp(mac:string, ip: string) {
+    async findByMacAndIp(mac: string, ip: string) {
         return await knex('Devices')
-            .where({ Mac: mac,
-                    Ip: ip
-             })
-             .select('*')
+            .where({
+                Mac: mac
+            })
+            .where({
+                Ip: ip
+            })
+            .select('*')
     }
 
-//     findBy(device: IDevice): any {
-//         return knex('devices').where(device).returning('*');
-//     }
+    //     findBy(device: IDevice): any {
+    //         return knex('devices').where(device).returning('*');
+    //     }
 
-//     findAll() {
-//         return knex('devices').returning('*');
-//     }
+    //     findAll() {
+    //         return knex('devices').returning('*');
+    //     }
 
-//     getAll(options: ISearchOpt) {
-//         return knex.raw(`SELECT *
-//             FROM devices
-//             WHERE mac_address LIKE '%${options.needle}%'
-//             OR dns_name_auto LIKE '%${options.needle}%'
-//             OR dns_name_manual LIKE '%${options.needle}%'
-//             ORDER BY created_at DESC
-//             LIMIT ${options.itemsPerPage}
-//             OFFSET ${(options.itemsPerPage * (options.activePage - 1))}`
-//         )
-//             .then((data: any) => {
-//                 return data;
-//             }).catch((err: any) => {
-//                 return err;
-//             });
-//     }
- }
+    //     getAll(options: ISearchOpt) {
+    //         return knex.raw(`SELECT *
+    //             FROM devices
+    //             WHERE mac_address LIKE '%${options.needle}%'
+    //             OR dns_name_auto LIKE '%${options.needle}%'
+    //             OR dns_name_manual LIKE '%${options.needle}%'
+    //             ORDER BY created_at DESC
+    //             LIMIT ${options.itemsPerPage}
+    //             OFFSET ${(options.itemsPerPage * (options.activePage - 1))}`
+    //         )
+    //             .then((data: any) => {
+    //                 return data;
+    //             }).catch((err: any) => {
+    //                 return err;
+    //             });
+    //     }
+}
