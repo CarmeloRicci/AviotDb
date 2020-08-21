f992eff254729d6bc688e91d0fa34738347395f8
Carmelo Ricci <carmeloricci@MacBook-Pro-di-Carmelo.local>
2020-08-21 08:12:42 +0200

    ok

--- backend/src/stores/tenantStore.ts
+++ backend/src/stores/tenantStore.ts
@@ -48,7 +48,7 @@ export default class TenantStore {
 
         return knex ('Tenant')
             .where ({
-                User_id: id
+                Tenant_id: id
             })
             .select ('*')
 
