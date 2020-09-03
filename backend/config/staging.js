68c1ab471086f3f10bf8882ee385d92b37941c03
Carmelo Ricci <carmeloricci@MacBook-Pro-di-Carmelo.local>
2020-09-03 12:23:02 +0200

    ok

--- backend/config/staging.js
+++ backend/config/staging.js
@@ -15,13 +15,16 @@ module.exports = {
   },
   general: {
     ipDnsServer: "10.10.0.1",
-    tenant_id: "Agri01",
-    ipIpDb: "127.0.0.1",
+    //tenant_id: "Agri01",
+    //ipIpDb: "127.0.0.1",
+    ipIpDb: process.env.IPDb,
     path_to_log: "/root/log.txt",
     path_to_log_ok: "/root/log_ok.txt",
     path_to_log_err: "/root/log_err.txt", 
-    ipFrontend: "172.19.0.5",
-    ipDnsServerApp: "172.19.0.6",
+    //ipFrontend: "172.19.0.5",
+    ipFrontend: process.env.IPFrontend,
+    ipDnsServerApp: process.env.IPDnsServerApp,
+    //ipDnsServerApp: "172.19.0.6",
     portDnsServerApp: "3900",
     portFrontend: "4100"
   }
